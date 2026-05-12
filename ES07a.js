8783334beffceb6a266b20796689e8c2e22f66a1
leggendar <bassettiloris200@gmail.com>
2026-05-12 14:44:26 +0200

    Create ES07a.js

---
+++ ES07a.js
@@ -0,0 +1,5 @@
+window.document: //Rappresenta il documento caricato nella finestra e fornisce metodi per manipolarlo.
+window.location: //Fornisce informazioni sull'URL del documento corrente e permette la navigazione verso nuovi URL.
+window.navigator: //Contiene informazioni sul browser e l'ambiente in cui il codice viene eseguito.
+window.innerWidth/window.innerHeight: //Restituiscono le dimensioni interne della finestra del browser.
+window.localStorage/window.sessionStorage: //Forniscono la possibilità di memorizzare dati localmente nel browser.
